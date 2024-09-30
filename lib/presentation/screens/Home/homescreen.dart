23494eb12cf2596496184ffaaa2527af1d7b498e
Ferdy R Cardona <ferdycard0na11@gmail.com>
2024-09-29 18:48:28 -0600

    Search responsive created

--- lib/presentation/screens/Home/homescreen.dart
+++ lib/presentation/screens/Home/homescreen.dart
@@ -1,52 +1,99 @@
 import 'package:cardsapps/presentation/screens/Home/AddCat.modal.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:go_router/go_router.dart'; // Para la navegación
+import 'package:go_router/go_router.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
-  // Método para obtener las categorías desde Firebase usando un stream
-  Stream<List<Map<String, dynamic>>> _streamCategories() {
-  return FirebaseFirestore.instance
-      .collection('categories')
-      .orderBy('timestamp', descending: true) // Ordenar por timestamp
-      .snapshots()
-      .map((snapshot) {
-    return snapshot.docs.map((doc) {
-      final data = doc.data();
-      data['id'] = doc.id; // Agregar el ID del documento
-      return data;
-    }).toList();
-  });
+  @override
+  State<HomePage> createState() => _HomePageState();
 }
 
+class _HomePageState extends State<HomePage> {
+  List<Map<String, dynamic>> _allResults = [];
+  List<Map<String, dynamic>> _resultsList = [];
+  final TextEditingController _searchController = TextEditingController();
+  final FocusNode _searchFocusNode = FocusNode();
+  bool _isSearchFocused = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _searchController.addListener(_onSearchChanged);
+    _searchFocusNode.addListener(_onFocusChanged);
+    getClientStream();
+  }
+
+  _onSearchChanged() {
+    searchResultList();
+  }
+
+  _onFocusChanged() {
+    setState(() {
+      _isSearchFocused = _searchFocusNode.hasFocus;
+    });
+  }
+
+  searchResultList() {
+    var showResults = <Map<String, dynamic>>[];
+    if (_searchController.text.isNotEmpty) {
+      showResults = _allResults.where((element) =>
+          element['nombre']
+              .toString()
+              .toLowerCase()
+              .contains(_searchController.text.toLowerCase())).toList();
+    } else {
+      showResults = List<Map<String, dynamic>>.from(_allResults);
+    }
+    setState(() {
+      _resultsList = showResults;
+    });
+  }
+
+  getClientStream() async {
+    var data = await FirebaseFirestore.instance
+        .collection('categories')
+        .orderBy('nombre')
+        .get();
+    setState(() {
+      _allResults = data.docs.map((doc) {
+        final data = doc.data() as Map<String, dynamic>;
+        data['id'] = doc.id;
+        return data;
+      }).toList();
+      _resultsList = List<Map<String, dynamic>>.from(_allResults);
+    });
+  }
+
+  @override
+  void dispose() {
+    _searchController.removeListener(_onSearchChanged);
+    _searchFocusNode.removeListener(_onFocusChanged);
+    _searchController.dispose();
+    _searchFocusNode.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[100],
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: StreamBuilder<List<Map<String, dynamic>>>(
-            stream: _streamCategories(),
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return const Center(child: CircularProgressIndicator());
-              } else if (snapshot.hasError) {
-                return const Center(child: Text('Error al cargar las categorías'));
-              } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                return const Center(child: Text('No hay categorías disponibles'));
-              }
-
-              // Lista de categorías obtenida de Firebase
-              final categorias = snapshot.data!;
-
-              return Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
+        child: GestureDetector(
+          onTap: () {
+            FocusScope.of(context).unfocus(); // Ocultar teclado al tocar fuera
+          },
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                AnimatedContainer(
+                  duration: const Duration(milliseconds: 300),
+                  height: _isSearchFocused ? 0 : 170.h, // Ajusta la altura cuando el foco está en el campo de búsqueda
+                  child: Padding(
                     padding: const EdgeInsets.all(20),
                     child: Row(
                       children: [
@@ -54,7 +101,10 @@ class HomePage extends StatelessWidget {
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
                             Text('Hola,', style: TextStyle(fontSize: 20.sp)),
-                            Text('Luis 👋', style: TextStyle(fontSize: 40.sp, fontWeight: FontWeight.bold)),
+                            Text('Luis 👋',
+                                style: TextStyle(
+                                    fontSize: 40.sp,
+                                    fontWeight: FontWeight.bold)),
                           ],
                         ),
                         const Spacer(),
@@ -62,141 +112,170 @@ class HomePage extends StatelessWidget {
                       ],
                     ),
                   ),
-                  Padding(
+                ),
+                AnimatedContainer(
+                  duration: const Duration(milliseconds: 300),
+                  height: _isSearchFocused ? 0 : 100.h, // Ajusta la altura cuando el foco está en el campo de búsqueda
+                  child: Padding(
                     padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.start,
                       children: [
-                        Text('¡Explora tu creatividad!', style: TextStyle(fontSize: 40.sp, fontWeight: FontWeight.bold)),
+                        Text('¡Explora tu creatividad!',
+                            style: TextStyle(
+                                fontSize: 40.sp, fontWeight: FontWeight.bold)),
                       ],
                     ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.all(20),
-                    child: TextField(
-                      decoration: InputDecoration(
-                        hintText: 'Buscar',
-                        prefixIcon: const Icon(Icons.search),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                          borderSide: BorderSide.none,
-                        ),
-                        filled: true,
-                        fillColor: Colors.grey[200],
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey[200],
+                      borderRadius: BorderRadius.circular(10),
+                      shape: BoxShape.rectangle,
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: CupertinoSearchTextField(
+                        controller: _searchController,
+                        focusNode: _searchFocusNode,
+                        onChanged: (value) {
+                          _onSearchChanged();
+                        },
+                        placeholder: 'Buscar',
+                        suffixIcon: const Icon(Icons.cancel_sharp),
+                        backgroundColor: Colors.grey[200],
+                        borderRadius: BorderRadius.circular(10),
                       ),
                     ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
-                    child: Row(
-                      children: [
-                        Text('Categorías', style: TextStyle(fontSize: 30.sp, fontWeight: FontWeight.bold)),
-                        const Spacer(),
-                        Text('Ver todo', style: TextStyle(fontSize: 22.sp, color: Colors.blue, fontWeight: FontWeight.bold)),
-                      ],
-                    ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
+                  child: Row(
+                    children: [
+                      Text('Categorías',
+                          style: TextStyle(
+                              fontSize: 30.sp, fontWeight: FontWeight.bold)),
+                      const Spacer(),
+                      Text('Ver todo',
+                          style: TextStyle(
+                              fontSize: 22.sp,
+                              color: Colors.blue,
+                              fontWeight: FontWeight.bold)),
+                    ],
                   ),
-                  // ListView horizontal que muestra las categorías de Firebase
-                  Container(
-                    height: 400.h,
-                    padding: const EdgeInsets.only(left: 20),
-                    child: ListView.builder(
-                      scrollDirection: Axis.horizontal,
-                      itemCount: categorias.length,
-                      itemBuilder: (context, index) {
-                        final categoria = categorias[index];
-                        return Container(
-                          width: 300.w,
-                          margin: const EdgeInsets.only(right: 16.0),
-                          decoration: BoxDecoration(
-                            color: Colors.teal[(index % 9 + 1) * 100],
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          child: Center(
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.spaceAround,
-                              children: [
-                                const Row(
-                                  mainAxisAlignment: MainAxisAlignment.end,
-                                  children: [
-                                    CircleAvatar(
-                                      radius: 20,
-                                      backgroundColor: Colors.white,
-                                      child: Icon(
-                                        Icons.favorite_rounded,
-                                        color: Color.fromARGB(255, 239, 148, 94),
-                                      ),
-                                    ),
-                                    SizedBox(width: 10),
-                                  ],
-                                ),
-                                const SizedBox(height: 10),
-                                Container(
-                                  height: 200.h,
-                                  width: 270.w,
-                                  decoration: BoxDecoration(
-                                    color: Colors.white,
-                                    borderRadius: BorderRadius.circular(10),
-                                  ),
-                                  child: Container(
-                                    margin: const EdgeInsets.all(10),
-                                    child: Column(
-                                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                                      crossAxisAlignment: CrossAxisAlignment.center,
-                                      children: [
-                                        Text(categoria['nombre'] ?? 'Sin título',
-                                            style: TextStyle(fontSize: 20.sp, fontWeight: FontWeight.bold)),
-                                        const SizedBox(height: 10),
-                                        ElevatedButton(
-                                          onPressed: () {
-                                            // Navegar a la pantalla de detalles de categoría
-                                            context.go('/Home/categoria/${categoria['id']}');
-                                          },
-                                          style: ElevatedButton.styleFrom(
-                                            backgroundColor: const Color.fromARGB(255, 239, 148, 94),
-                                            shape: RoundedRectangleBorder(
-                                              borderRadius: BorderRadius.circular(10),
-                                            ),
-                                          ),
-                                          child: Text('Ver tarjeta', style: TextStyle(fontSize: 20.sp, color: Colors.white)),
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        );
-                      },
-                    ),
-                  ),
-                  SizedBox(height: 60.h),
-                  Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: GestureDetector(
-                      onTap: () {
-                        showModalBottomSheet(
-                          context: context, 
-                          builder: (ctx) => const AddCatmod(),
-                          isScrollControlled: true, 
-                        );
-                      },
-                      child: Container(
-                        height: 100.h,
+                ),
+                Container(
+                  height: 400.h,
+                  padding: const EdgeInsets.only(left: 20),
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: _resultsList.length,
+                    itemBuilder: (context, index) {
+                      final categoria = _resultsList[index];
+                      return Container(
+                        width: 300.w,
+                        margin: const EdgeInsets.only(right: 16.0),
                         decoration: BoxDecoration(
-                          color: const Color.fromARGB(255, 239, 148, 94),
+                          color: Colors.teal[(index % 9 + 1) * 100],
                           borderRadius: BorderRadius.circular(10),
                         ),
-                        child: const Center(
-                          child: Text('Crear nueva tarjeta', style: TextStyle(fontSize: 24, color: Colors.white)),
+                        child: Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              const Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  CircleAvatar(
+                                    radius: 20,
+                                    backgroundColor: Colors.white,
+                                    child: Icon(
+                                      Icons.favorite_rounded,
+                                      color: Color.fromARGB(255, 239, 148, 94),
+                                    ),
+                                  ),
+                                  SizedBox(width: 10),
+                                ],
+                              ),
+                              const SizedBox(height: 10),
+                              Container(
+                                height: 200.h,
+                                width: 270.w,
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                child: Container(
+                                  margin: const EdgeInsets.all(10),
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                    crossAxisAlignment: CrossAxisAlignment.center,
+                                    children: [
+                                      Text(categoria['nombre'] ?? 'Sin título',
+                                          style: TextStyle(
+                                              fontSize: 20.sp,
+                                              fontWeight: FontWeight.bold)),
+                                      const SizedBox(height: 10),
+                                      ElevatedButton(
+                                        onPressed: () {
+                                          context.go(
+                                              '/Home/categoria/${categoria['id']}');
+                                        },
+                                        style: ElevatedButton.styleFrom(
+                                          backgroundColor: const Color.fromARGB(
+                                              255, 239, 148, 94),
+                                          shape: RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(10),
+                                          ),
+                                        ),
+                                        child: Text('Ver tarjeta',
+                                            style: TextStyle(
+                                                fontSize: 20.sp,
+                                                color: Colors.white)),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
+                      );
+                    },
+                  ),
+                ),
+                // Espacio extra para que el teclado no cubra el contenido
+                SizedBox(height: 60.h),
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: GestureDetector(
+                    onTap: () {
+                      showModalBottomSheet(
+                        context: context,
+                        builder: (ctx) => const AddCatmod(),
+                        isScrollControlled: true,
+                      );
+                    },
+                    child: Container(
+                      height: 100.h,
+                      decoration: BoxDecoration(
+                        color: const Color.fromARGB(255, 239, 148, 94),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: const Center(
+                        child: Text('Crear nueva tarjeta',
+                            style: TextStyle(fontSize: 24, color: Colors.white)),
                       ),
                     ),
-                  )
-                ],
-              );
-            },
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
